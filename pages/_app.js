6fc6d9237252d9547533150eda3e0868d72105f0
kensidiangco <kensidiangco@gmail.com>
2022-10-10 19:33:24 +0800

    fixing layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,29 @@
 import Footer from '../components/Footer'
 import Navigation from '../components/Navigation'
 import '../styles/globals.css'
+import { motion } from 'framer-motion';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
-    <div className='bg-gray-50 font-mono'>
-      <Navigation />
-      <Component {...pageProps} />
-      <Footer />
-    </div>
+    <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" exit="pageExit" variants={{
+      pageInitial: {
+        opacity: 0
+      },
+      pageAnimate: {
+        opacity: 1
+      },
+      pageExit: {
+        backgroundColor: 'white',
+        filter: `invert()`,
+        opacity: 0
+      }
+    }}>
+      <div className='bg-gray-50 font-mono'>
+        <Navigation />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </motion.div>
   )
 }
 
